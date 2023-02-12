9544f1669ccf1575bd16ef1db69f9cad387b9c47
Abdelrahman Shawky <abdelrahman.a.shawky@gmail.com>
2023-02-12 18:02:24 +0200

    pug template engine

--- app.js
+++ app.js
@@ -5,8 +5,16 @@ const adminData = require('./routes/admin.js');
 const shopRoutes = require('./routes/shop.js');
 const path = require('path');
 
-
 const app = express();
+// set gloabal config value
+// can read them through app.get
+// share across application
+// use engine already installed
+// built in express support
+app.set('view engine', 'pug');
+// set to views folder
+app.set('views', 'views');
+
 
 app.use(bodyParser.urlencoded({extended: false}));
 // serve static files
@@ -19,7 +27,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views','404.html'));
+    res.status(404).render('404.pug', {pageTitle: '404'});
 });
     
 app.listen(3000)
--- routes/shop.js
+++ routes/shop.js
@@ -8,13 +8,9 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) =>{
-    console.log('In main moiddleware');
-    console.log('shop.js',adminData.products);
-    // import path correctly
-    // global variable holding abs file path
-    // join doesn't use slashes
-    // ../ to go up one level
-    res.sendFile(path.join(rootDir, 'views','shop.html'));
+    const products = adminData.products;
+    // already defined folder views and pug
+    res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'}); 
 });
 
 module.exports = router;
