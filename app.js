5c2ffb46b5b8e8b6a1ecbe7214c06521a4df9511
Abdelrahman Shawky <abdelrahman.a.shawky@gmail.com>
2023-02-22 22:17:28 +0200

    Mongoose setup

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const adminRoutes = require('./routes/admin.js');
 const shopRoutes = require('./routes/shop.js');
 const path = require('path');
 const errorController = require('./controllers/error');
-const MongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
+// const MongoConnect = require('./util/database').mongoConnect;
+// const User = require('./models/user');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -15,33 +16,23 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use((req,res,next) => {
-    User.findById('63f53753eefdcc4712dcd4dd')
-    .then(user => {
-        // allow to use user methods
-        req.user = new User(user.name, user.email, user.cart, user._id);
-        next();
-    })
-    .catch(err => console.log(err));
-    // next();
-});
+// app.use((req,res,next) => {
+//     User.findById('63f53753eefdcc4712dcd4dd')
+//     .then(user => {
+//         // allow to use user methods
+//         req.user = new User(user.name, user.email, user.cart, user._id);
+//         next();
+//     })
+//     .catch(err => console.log(err));
+//     // next();
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(errorController.get404);
 
-MongoConnect(() =>{
-    // User.findById(1)
-    // .then(user => {
-    //     if(!user){
-    //         user = new User('admin', 'admin@admin.com');
-    //         user.save()
-    //     }
-    //     return Promise.resolve(user);
-    // })
-    // .then(user => {
-    //     console.log(user);
-    // })
-    // .catch(err => console.log(err));
-    app.listen(3000); 
-});
+mongoose.connect('mongodb+srv://abdelrahmanshawky:nuUTJaF9KJQqSrUE@cluster0.cgns5uz.mongodb.net/shop?retryWrites=true&w=majority')
+.then(result => {
+    app.listen(3000);
+})
+.catch(err => console.log(err));
