13407b9604640ee845fefa5e44b94763f70cff02
Abdelrahman Shawky <abdelrahman.a.shawky@gmail.com>
2023-03-09 17:01:33 +0200

    pagination complete

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,16 +1,32 @@
 const Product = require('../models/product');
 const mongoose = require('mongoose');
 const Order = require('../models/order');
-const ITEMS_PER_PAGE = 2;
+const ITEMS_PER_PAGE = 1;
 
 
 exports.getProducts = (req, res, next) =>{
-    Product.find()
+    const page = +req.query.page || 1;
+    let totalItems;
+    Product.find().countDocuments()
+    .then(numberProducts => {
+        totalItems = numberProducts
+        return Product.find()
+        .skip((page-1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE)
+    })
     .then(products => {
+        console.log(products);
         res.render('shop/product-list', {
             prods: products, 
             pageTitle: 'All Products', 
-            path: '/products'
+            path: '/products',
+            csrfToken: req.csrfToken(),
+            currentPage: page,
+            hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+            hasPreviousPage: page > 1,
+            nextPage: page + 1,
+            previousPage: page - 1,
+            lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
         });
     })
     .catch(err => {
@@ -21,24 +37,38 @@ exports.getProducts = (req, res, next) =>{
 };
 
 exports.getIndex =(req,res, next) => {
-    const page = req.query.page;
-    console.log(req.session.isLoggedIn);
-    Product.find()
-    .skip((page-1) * ITEMS_PER_PAGE)
-    .limit(ITEMS_PER_PAGE)
+    const page = +req.query.page || 1;
+    let totalItems;
+    Product.find().countDocuments()
+    .then(numberProducts => {
+        totalItems = numberProducts
+        return Product.find()
+        .skip((page-1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE)
+    })
     .then(products => {
+        console.log(products);
         res.render('shop/index', {
             prods: products, 
             pageTitle: 'Shop', 
             path: '/',
-            csrfToken: req.csrfToken()
+            csrfToken: req.csrfToken(),
+            currentPage: page,
+            hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+            hasPreviousPage: page > 1,
+            nextPage: page + 1,
+            previousPage: page - 1,
+            lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
         });
     })
     .catch(err => {
-        const error = new Error(err);
-        error.httpStatuseCode = 500;
-        return next(error); //skip all middleware till error handling middleware
+        console.log(err)
+        // const error = new Error(err);
+        // error.httpStatuseCode = 500;
+        // return next(error); //skip all middleware till error handling middleware
     });
+
+    
 };
 
 exports.getProduct =(req,res, next) => {
